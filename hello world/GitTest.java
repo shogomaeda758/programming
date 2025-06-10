e23bb401ae225f81e7ee8d7df1efe90ad1dacf4a
shogo.maeda <shogo.maeda@avantcorp.com>
2025-06-10 17:28:33 +0900

    Gitの課題

--- hello world/GitTest.java
+++ hello world/GitTest.java
@@ -1,5 +1,5 @@
 public class GitTest {
     public static void main(String[] args) throws Exception {
-        System.out.println("コミットからプッシュまでを行おう");
+        System.out.println("コミットからプッシュまでを行おう！");
     }
 }
